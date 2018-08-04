c0ea84800772612b8b6ecf47bc05e0b861a36060
Johnynoxwell <jimboo19898@gmail.com>
2018-08-04 19:04:55 +0300

    final

--- gulpfile.js
+++ gulpfile.js
@@ -39,6 +39,10 @@ gulp.task('common-js', function() {
 gulp.task('js', ['common-js'], function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
+        'app/libs/parallax.js',
+        'app/libs/parallax1.js',
+        'app/libs/contact-form.js',
+        'app/libs/parsley.js',
 		'app/js/common.min.js', // Всегда в конце
 		])
 	.pipe(concat('scripts.min.js'))
